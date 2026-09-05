215af34d3a306f3f1e35e8ccf9e16001457f1ba2
zaidbh195-afk <zaidbh195@gmail.com>
2026-09-05 11:45:15 +0530

    Create physics.js with basic physics variables

---
+++ physics.js
@@ -0,0 +1,17 @@
+const mass = 1200;
+
+let force = 0;
+
+let acceleration = 0;
+
+let velocity = 0;
+
+let position = 0;
+
+export {
+    mass,
+    force,
+    acceleration,
+    velocity,
+    position
+};
