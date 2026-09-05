d0bd769107b6249331ebab7067a22f130006bf61
zaidbh195-afk <zaidbh195@gmail.com>
2026-09-05 11:43:36 +0530

    Initialize Three.js scene with camera and ground

---
+++ scene.js
@@ -0,0 +1,47 @@
+import * as THREE from
+"https://cdn.jsdelivr.net/npm/three@0.179.1/build/three.module.js";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(
+    window.innerWidth,
+    window.innerHeight
+);
+
+document.body.appendChild(renderer.domElement);
+
+camera.position.set(6, 4, 8);
+
+camera.lookAt(0, 0, 0);
+
+const groundGeometry = new THREE.PlaneGeometry(100, 100);
+
+const groundMaterial = new THREE.MeshBasicMaterial({
+    color: 0x555555
+});
+
+const ground = new THREE.Mesh(
+    groundGeometry,
+    groundMaterial
+);
+
+ground.rotation.x = -Math.PI / 2;
+
+scene.add(ground);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+}
+
+animate();
